872174b447f4dfd40fffefdd165c4f719685e1eb
mihneavlad <mihnel@yahoo.com>
2018-12-09 13:10:53 +0100

    Created 4 mock tasks similar to the json file

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,8 @@
+import { Task } from "./task";
+
+export const TASKS: Task[] = [
+  { id: 0, name: "Mr. Nice" },
+  { id: 1, name: "Narco" },
+  { id: 2, name: "Bombasto" },
+  { id: 3, name: "Celeritas" }
+];
