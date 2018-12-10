952ed98abbf86e883b985530e02f889da0155b11
mihneavlad <mihnel@yahoo.com>
2018-12-10 17:02:43 +0100

    Added routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from "./app.component";
 import { UiModule } from "./ui/ui.module";
 import { TasksComponent } from "./tasks/tasks.component";
 import { TaskDetailComponent } from "./task-detail/task-detail.component";
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, TasksComponent, TaskDetailComponent],
-  imports: [BrowserModule, UiModule],
+  imports: [BrowserModule, UiModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
