90d44b590cdc14a6c65fe64bb680adb06a72dc9d
mihneavlad <mihnel@yahoo.com>
2018-12-13 17:24:14 +0100

    ERROR when rendering detail view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ import { MainComponent } from "./main/main.component";
 import { TaskDetailComponent } from "./task-detail/task-detail.component";
 
 const routes: Routes = [
-  { path: "", redirectTo: "/main", pathMatch: "full" },
-  { path: "main", component: MainComponent },
+  { path: "", redirectTo: "/tasks", pathMatch: "full" },
+  // { path: "main", component: MainComponent },
   { path: "task/:id", component: TaskDetailComponent },
   { path: "tasks", component: TasksComponent }
 ];
