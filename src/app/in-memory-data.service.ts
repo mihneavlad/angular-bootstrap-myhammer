9db7ae3aef41244eeca2416ec1e29fbb1e29aa08
mihneavlad <mihnel@yahoo.com>
2018-12-16 16:11:30 +0100

    Fetched json data

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,22 +1,26 @@
 import { Injectable } from "@angular/core";
 import { InMemoryDbService } from "angular-in-memory-web-api";
 import { Task } from "./task";
+import jobs from "../assets/data/jobs.json";
 
 @Injectable({
   providedIn: "root"
 })
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
-    const tasks = [
-      { id: 0, title: "Dach" },
-      { id: 1, title: "Scheiben" },
-      { id: 2, title: "Holz" },
-      { id: 3, title: "Reinigung" }
-    ];
-    return { tasks };
+    const tasks = async () => {
+      const response = await fetch("../assets/data/jobs.json");
+      const json = await response.json();
+      console.log(json);
+      console.log(json.body);
+    };
+
+    tasks();
+
+    // return { tasks };
   }
 
-  genId(tasks: Task[]): number {
-    return tasks.length > 0 ? Math.max(...tasks.map(task => task.id)) + 1 : 0;
-  }
+  // genId(tasks: Task[]): number {
+  //   return tasks.length > 0 ? Math.max(...tasks.map(task => task.id)) + 1 : 0;
+  // }
 }
